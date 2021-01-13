07ea45eb777e947db44be5cc44ebe3e2ecede207
miwzerking <0865697@students.dadeschools.net>
2021-01-13 16:04:54 -0500

    completed header and folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import Home from './pages/Home';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from 'react-router-dom';
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>search engine</h1>
+			<Router>
+				<Switch>
+					<Route path='/search'>
+						<h1>search page</h1>
+					</Route>
+
+					<Route path='/'>
+						<Home />
+					</Route>
+				</Switch>
+			</Router>
 		</div>
 	);
 }
